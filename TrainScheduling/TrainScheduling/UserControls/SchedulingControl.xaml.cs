8da5f800335ff33d0a9c97921f919659795234e5
Shuxiao Wang <keaising@gmail.com>
2016-06-16 01:12:29 +0800

    添加边框和动态切换Tab效果

--- TrainScheduling/TrainScheduling/UserControls/SchedulingControl.xaml.cs
+++ TrainScheduling/TrainScheduling/UserControls/SchedulingControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TrainScheduling.UserControls;
 
-namespace TrainScheduling
+namespace TrainScheduling.UserControls
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SchedulingControl.xaml
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class SchedulingControl : UserControl
     {
-        public MainWindow()
+        public SchedulingControl()
         {
             InitializeComponent();
         }
